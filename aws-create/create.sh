faeacda1908d786e933bf4e08e861541f07dc5e7
naveen <naveen@gmail.com>
2021-07-22 13:29:49 +0530

    hosted

--- aws-create/create.sh
+++ aws-create/create.sh
@@ -24,7 +24,7 @@ Instance_Create() {
   IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z07094733BECKU87XMIZ3 --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z043741825APTAO8FY413 --change-batch file:///tmp/record.json
   sed -i -e "/${COMPONENT}/ d" ../ips
   echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../ips
 }
